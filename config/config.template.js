376707b97d5153c953cb1b659c950373f44947ad
NullDev (Shadow) <chris@nulldev.org>
2024-09-24 00:31:24 +0200

    removed hardcoded vars

--- config/config.template.js
+++ config/config.template.js
@@ -9,7 +9,5 @@ export default {
         name: "Your Name",
         email: "your-email@example.com",
     },
-    local_repo_path: "./local_repo",
-    processed_shas_file: "./processed_shas.json",
     repos_to_exclude: [],
 };
