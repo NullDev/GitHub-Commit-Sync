34472196c2ad33d03ea07b2f9f6a1e620163fcea
NullDev (Shadow) <chris@nulldev.org>
2024-09-23 21:44:05 +0200

    added config and template

---
+++ config/config.template.js
@@ -0,0 +1,15 @@
+export default {
+    auth: {
+        main_account_token: "",
+        secondary_account_token: "",
+    },
+    sync_repo_owner: "",
+    sync_repo_name: "",
+    author: {
+        name: "Your Name",
+        email: "your-email@example.com",
+    },
+    local_repo_path: "./local_repo",
+    processed_shas_file: "./processed_shas.json",
+    repos_to_exclude: [],
+};
